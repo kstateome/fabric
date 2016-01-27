28eb1b90cc3226f1c6cb11a3ba732344dc5985d8
Aaron Schif <aaronschif@gmail.com>
2016-01-27 12:01:48 -0600

    Fix exceptions

--- fabric/thread_handling.py
+++ fabric/thread_handling.py
@@ -7,12 +7,14 @@ class ThreadHandler(object):
     def __init__(self, name, callable, *args, **kwargs):
         # Set up exception handling
         self.exception = None
+        self.exc_info = None
 
         def wrapper(*args, **kwargs):
             try:
                 callable(*args, **kwargs)
             except BaseException as e:
                 self.exception = e
+                self.exc_info = sys.exc_info()
         # Kick off thread
         thread = threading.Thread(None, wrapper, name, args, kwargs)
         thread.setDaemon(True)
@@ -22,4 +24,4 @@ class ThreadHandler(object):
 
     def raise_if_needed(self):
         if self.exception:
-            six.reraise(self.exception)
+            six.reraise(*self.exc_info)
